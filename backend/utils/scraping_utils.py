c7747d44a8258a710f571df8a540cba73c0f8d88
Ajaypal-Singh-Gill <ajaygillsudhar@gmail.com>
2024-11-02 15:06:44 -0400

    Self Review-2

--- backend/utils/scraping_utils.py
+++ backend/utils/scraping_utils.py
@@ -10,7 +10,6 @@ def run_scrapy_spider(url):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         scrapy_script = os.path.join(current_dir, '../scrapy/run_scrapy.py')
         
-        # Normalize the path to handle different operating systems
         scrapy_script = os.path.normpath(scrapy_script)
         subprocess.run(['python', scrapy_script, url], check=True)
     except subprocess.CalledProcessError as e:
