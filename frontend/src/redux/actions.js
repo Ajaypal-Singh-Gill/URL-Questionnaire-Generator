780d42f93755cebd2115d89024cb6f2f0b725d6f
Ajaypal-Singh-Gill <ajaygillsudhar@gmail.com>
2024-11-22 01:40:53 -0500

    Correct Base URL

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BE_BASE_URL || "http://localhost:5001";
+const BASE_URL = process.env.REACT_APP_BE_BASE_URL || "http://localhost:5001";
 
 export const generateQuestion = (url) => async (dispatch) => {
   try {
